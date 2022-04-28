565989771c0b31b982bcccfe90d5007fdb27dc29
Raphael de Mello - RDM <raph.mello@gmail.com>
2022-04-28 10:21:24 -0300

    create project

---
+++ src/main/java/com/raje/smartsplit/SmartsplitApplication.java
@@ -0,0 +1,13 @@
+package com.raje.smartsplit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartsplitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartsplitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raje/smartsplit/SmartsplitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raje.smartsplit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartsplitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
