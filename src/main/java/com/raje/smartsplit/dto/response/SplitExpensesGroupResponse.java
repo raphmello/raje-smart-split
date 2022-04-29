06cc9f138296431fe30014b44233d15bde6fc4b6
Raphael de Mello - RDM <raph.mello@gmail.com>
2022-04-29 09:52:10 -0300

    Adjust response

--- src/main/java/com/raje/smartsplit/dto/response/SplitExpensesGroupResponse.java
+++ src/main/java/com/raje/smartsplit/dto/response/SplitExpensesGroupResponse.java
@@ -1,6 +1,5 @@
 package com.raje.smartsplit.dto.response;
 
-import com.raje.smartsplit.entity.AppUser;
 import com.raje.smartsplit.entity.Bill;
 import com.raje.smartsplit.entity.SplitExpensesGroup;
 import lombok.AllArgsConstructor;
@@ -16,12 +15,10 @@ public class SplitExpensesGroupResponse {
     private Long id;
     private String title;
     private List<Bill> bills;
-    private List<AppUser> appUsers;
 
     public SplitExpensesGroupResponse(SplitExpensesGroup entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.bills = entity.getBills();
-        this.appUsers = entity.getAppUsers();
     }
 }
