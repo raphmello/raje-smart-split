31f4fa9b3daa64266885cc1c77909e1b8f4b4bec
Raphael de Mello - RDM <raph.mello@gmail.com>
2022-04-29 08:50:51 -0300

    Add attribute to dto

--- src/main/java/com/raje/smartsplit/dto/response/SplitExpensesGroupResponse.java
+++ src/main/java/com/raje/smartsplit/dto/response/SplitExpensesGroupResponse.java
@@ -1,5 +1,6 @@
 package com.raje.smartsplit.dto.response;
 
+import com.raje.smartsplit.entity.AppUser;
 import com.raje.smartsplit.entity.Bill;
 import com.raje.smartsplit.entity.SplitExpensesGroup;
 import lombok.AllArgsConstructor;
@@ -15,10 +16,12 @@ public class SplitExpensesGroupResponse {
     private Long id;
     private String title;
     private List<Bill> bills;
+    private List<AppUser> appUsers;
 
     public SplitExpensesGroupResponse(SplitExpensesGroup entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.bills = entity.getBills();
+        this.appUsers = entity.getAppUsers();
     }
 }
