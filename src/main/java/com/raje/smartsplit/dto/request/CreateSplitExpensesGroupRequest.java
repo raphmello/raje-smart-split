00ef12eb25c57db7e19ce37d86f7c182c4735a3c
Raphael de Mello - RDM <raph.mello@gmail.com>
2022-04-30 10:41:06 -0300

    New field and @NotNull validations

--- src/main/java/com/raje/smartsplit/dto/request/CreateSplitExpensesGroupRequest.java
+++ src/main/java/com/raje/smartsplit/dto/request/CreateSplitExpensesGroupRequest.java
@@ -5,10 +5,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateSplitExpensesGroupRequest {
+    @NotNull
     private String title;
+    @NotNull
+    private Long userId;
 }
