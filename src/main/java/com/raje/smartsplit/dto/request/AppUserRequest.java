02ed45f8025d5efcf9bab01c28decf54b39ca5dd
Raphael de Mello - RDM <raph.mello@gmail.com>
2022-04-30 10:38:38 -0300

    Creates AppUserRequest

---
+++ src/main/java/com/raje/smartsplit/dto/request/AppUserRequest.java
@@ -0,0 +1,17 @@
+package com.raje.smartsplit.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppUserRequest {
+    @NotNull
+    private Long userId;
+}
