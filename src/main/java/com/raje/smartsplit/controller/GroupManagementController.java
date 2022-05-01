5ac0e8cfb9059f1f58757f4597c6949b0d51724e
Raphael de Mello - RDM <raph.mello@gmail.com>
2022-05-01 18:32:22 -0300

    Refactor /group endpoints

--- src/main/java/com/raje/smartsplit/controller/GroupManagementController.java
+++ src/main/java/com/raje/smartsplit/controller/GroupManagementController.java
@@ -1,32 +1,63 @@
 package com.raje.smartsplit.controller;
 
+import com.raje.smartsplit.dto.request.CreateSplitExpensesGroupRequest;
 import com.raje.smartsplit.dto.response.SplitExpensesGroupResponse;
+import com.raje.smartsplit.dto.response.SplitExpensesGroupSimpleResponse;
+import com.raje.smartsplit.entity.SplitExpensesGroup;
 import com.raje.smartsplit.service.GroupManagementService;
+import com.raje.smartsplit.service.SplitExpensesGroupService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/groupManagement")
+@RequestMapping("api/group")
 public class GroupManagementController {
 
-    private final GroupManagementService service;
+    private final GroupManagementService groupManagementService;
+
+    private final SplitExpensesGroupService groupService;
 
     @Autowired
-    public GroupManagementController(GroupManagementService service) {
-        this.service = service;
+    public GroupManagementController(GroupManagementService groupManagementService, SplitExpensesGroupService groupService) {
+        this.groupManagementService = groupManagementService;
+        this.groupService = groupService;
     }
 
-    @PostMapping("/group/{groupId}/user/{userId}")
+    @GetMapping
+    @Operation(summary = "Retreive all groups (only id and name, not bills and participants)")
+    public ResponseEntity<List<SplitExpensesGroupSimpleResponse>> getAllGroups() {
+        List<SplitExpensesGroup> entityList = groupService.findAll();
+        List<SplitExpensesGroupSimpleResponse> responseList = new ArrayList<>();
+        entityList.forEach(group -> responseList.add(new SplitExpensesGroupSimpleResponse(group)));
+        return new ResponseEntity<>(responseList, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Find a group by id (includes all bills and participants)")
+    public ResponseEntity<SplitExpensesGroupResponse> getGroup(@PathVariable("id") Long groupId) {
+        SplitExpensesGroupResponse response = groupService.getGroupById(groupId);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PostMapping
+    @Operation(summary = "Create a group and includes the creator as a participant")
+    public ResponseEntity<SplitExpensesGroupResponse> createGroup(@RequestBody @Valid CreateSplitExpensesGroupRequest request) {
+        SplitExpensesGroup group = groupService.createGroup(request);
+        return new ResponseEntity<>(new SplitExpensesGroupResponse(group), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{groupId}/user/{userId}")
     @Operation(summary = "Add a user{userId} to the group{groupId}")
-    public ResponseEntity<SplitExpensesGroupResponse> addParticipant(@PathVariable(value = "groupId") Long groupId,
+    public ResponseEntity<SplitExpensesGroupResponse> addParticipantToGroup(@PathVariable(value = "groupId") Long groupId,
                                                                      @PathVariable(value = "userId") Long userId) {
-        SplitExpensesGroupResponse groupUpdated = service.addParticipantToGroup(userId,groupId);
+        SplitExpensesGroupResponse groupUpdated = groupManagementService.addParticipantToGroup(userId,groupId);
         return new ResponseEntity<>(groupUpdated, HttpStatus.CREATED);
     }
 }
