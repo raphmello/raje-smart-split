0a4d758189028e0d8338cd032ab0c78f6a9bc1c0
Raphael de Mello - RDM <raph.mello@gmail.com>
2022-05-01 20:04:50 -0300

    Add equals method to avoid add the same Participant in a group

--- src/main/java/com/raje/smartsplit/entity/Participant.java
+++ src/main/java/com/raje/smartsplit/entity/Participant.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @AllArgsConstructor
@@ -25,4 +26,20 @@ public class Participant {
 
     private Double splitShare = 1.;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Participant)) return false;
+
+        Participant that = (Participant) o;
+
+        return Objects.equals(user.getUsername(), that.user.getUsername());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = splitExpensesGroup != null ? splitExpensesGroup.hashCode() : 0;
+        result = 31 * result + (user != null ? user.hashCode() : 0);
+        return result;
+    }
 }
