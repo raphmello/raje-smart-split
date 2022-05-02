36d538f07c445ecb340ba2e0628cb2ac1fabb5b5
Raphael de Mello - RDM <raph.mello@gmail.com>
2022-05-02 13:32:38 -0300

    add local and prod server to open API

--- src/main/java/com/raje/smartsplit/config/OpenApi30/OpenApi30Config.java
+++ src/main/java/com/raje/smartsplit/config/OpenApi30/OpenApi30Config.java
@@ -5,21 +5,39 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class OpenApi30Config {
     @Bean
     public OpenAPI customOpenAPI() {
+
         return new OpenAPI()
                 .components(new Components().addSecuritySchemes("bearer-jwt",
                         new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
                                 .in(SecurityScheme.In.HEADER).name("Authorization")))
                 .info(new Info().title("App API").version("snapshot"))
                 .addSecurityItem(
-                        new SecurityRequirement().addList("bearer-jwt", Arrays.asList("read", "write")));
+                        new SecurityRequirement().addList("bearer-jwt", Arrays.asList("read", "write")))
+                .servers(createServers());
+    }
+
+    private List<Server> createServers() {
+        List<Server> servers = new ArrayList<>();
+        Server serverProd = new Server();
+        serverProd.setDescription("PROD");
+        serverProd.setUrl("https://aws.prod.smartsplit");
+        Server serverDev = new Server();
+        serverDev.setDescription("DEV");
+        serverDev.setUrl("http://localhost:8080");
+        servers.add(serverDev);
+        servers.add(serverProd);
+        return servers;
     }
 }
--- src/main/java/com/raje/smartsplit/config/SecurityConfig/WebSecurityConfig.java
+++ src/main/java/com/raje/smartsplit/config/SecurityConfig/WebSecurityConfig.java
@@ -58,8 +58,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
+                .antMatchers("/actuator/**").permitAll()
                 .antMatchers(HttpMethod.POST,"/user").permitAll()
-                .antMatchers("/swagger-ui/**","/v3/api-docs/**", "/javainuse-openapi/**").permitAll()
+                .antMatchers("/swagger-ui/**","/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated();
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
