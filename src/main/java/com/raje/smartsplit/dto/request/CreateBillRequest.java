9ee8f1cdd8148ea96980efc4568d3863ea12bdb3
Raphael de Mello - RDM <raph.mello@gmail.com>
2022-05-03 18:47:56 -0300

    add CreateBillRequest

---
+++ src/main/java/com/raje/smartsplit/dto/request/CreateBillRequest.java
@@ -0,0 +1,34 @@
+package com.raje.smartsplit.dto.request;
+
+import com.raje.smartsplit.entity.Bill;
+import com.raje.smartsplit.enums.CategoryEnum;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateBillRequest {
+    @NotBlank
+    private String description;
+
+    @NotNull
+    private CategoryEnum category = CategoryEnum.GENERAL;
+
+    @NotNull
+    private Double amount;
+
+    public Bill requestToEntity() {
+        Bill bill = new Bill();
+        bill.setDescription(this.description);
+        bill.setCategory(this.category);
+        bill.setAmount(this.amount);
+        return bill;
+    }
+}
