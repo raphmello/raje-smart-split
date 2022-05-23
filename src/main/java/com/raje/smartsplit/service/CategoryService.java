9c6be5b3b0783515a573d1faf2cc30bd7b1e9f07
Raphael de Mello - RDM <raph.mello@gmail.com>
2022-05-23 20:39:04 -0300

    search all categories, and search all distinct categories of a group

--- src/main/java/com/raje/smartsplit/service/CategoryService.java
+++ src/main/java/com/raje/smartsplit/service/CategoryService.java
@@ -4,10 +4,10 @@ import com.raje.smartsplit.entity.BillCategory;
 import com.raje.smartsplit.exception.CategoryNotFoundException;
 import com.raje.smartsplit.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
-import org.webjars.NotFoundException;
 
-import java.util.List;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class CategoryService {
@@ -25,7 +25,11 @@ public class CategoryService {
         return optional.get();
     }
 
-    public List<BillCategory> findAll() {
-        return repository.findAll();
+    public Set<BillCategory> findAll() {
+        return new HashSet<>(repository.findAll());
+    }
+
+    public Set<BillCategory> findAllByGroupId(Long groupId) {
+        return repository.findAllByGroupId(groupId);
     }
 }
